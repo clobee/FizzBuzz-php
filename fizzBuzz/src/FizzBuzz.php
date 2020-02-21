aba127c6d4fd9cc739387b2b3068eaa3ca729b2e
Claudius_XVIII <cbindika@gmail.com>
2020-02-21 02:09:07 +0000

    Adds class + test #fizzbuzz

--- fizzBuzz/src/FizzBuzz.php
+++ fizzBuzz/src/FizzBuzz.php
@@ -2,9 +2,29 @@
 
 declare(strict_types=1);
 
-namespace PHP_Exercices;
+namespace php_exercices;
 
 final class FizzBuzz
 {
+    public function process(
+        int $number,
+        int $fizzNumber = 3,
+        int $buzzNumber = 5
+    ) : string {
+        $output = '';
 
+        if ($number%$fizzNumber == 0) {
+            $output .= 'Fizz';
+        }
+
+        if ($number%$buzzNumber == 0) {
+            $output .= 'Buzz';
+        }
+
+        if ($output == '') {
+            $output = "$number";
+        }
+
+        return $output;
+    }
 }
--- fizzBuzz/tests/FizzBuzzTest.php
+++ fizzBuzz/tests/FizzBuzzTest.php
@@ -4,6 +4,50 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
+use function PHPUnit\Framework\assertEquals;
+
 final class FizzBuzzTest extends TestCase
 {
+    /**
+     * @dataProvider provideArrayData()
+     */
+    public function test_fizz_buzz_is_returned(
+        int $key,
+        string $value
+    ) {
+        $result = new php_exercices\FizzBuzz();
+
+        assertEquals(
+            $result->process($key),
+            $value,
+            "{$key} should return {$value}"
+        );
+    }
+
+    public function provideArrayData() : array
+    {
+        return [
+            [ 0 , 'FizzBuzz'],
+            [ 1 , '1'],
+            [ 2 , '2'],
+            [ 3 , 'Fizz'],
+            [ 4 , '4'],
+            [ 5 , 'Buzz'],
+            [ 6 , 'Fizz'],
+            [ 7 , '7'],
+            [ 8 , '8'],
+            [ 9 , 'Fizz'],
+            [ 10 , 'Buzz'],
+            [ 11 , '11'],
+            [ 12 , 'Fizz'],
+            [ 13 , '13'],
+            [ 14 , '14'],
+            [ 15 , 'FizzBuzz'],
+            [ 16 , '16'],
+            [ 17 , '17'],
+            [ 18 , 'Fizz'],
+            [ 19 , '19'],
+            [ 20 , 'Buzz']
+        ];
+    }
 }
